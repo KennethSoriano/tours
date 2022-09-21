c6e7c59a4e0adb13fa4beccc70bcea65bfe71371
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2022-09-20 20:21:13 -0700

    reset

--- src/Tours.js
+++ src/Tours.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import Tour from './Tour';
-const Tours = ({ tours, removeTour }) => {
+const Tours = () => {
   return (
     <section>
-      <div className="title">
-        <h2>our tours</h2>
-        <div className="underline"></div>
-      </div>
-      <div>
-        {tours.map((tour) => {
-          return <Tour key={tour.id} {...tour} removeTour={removeTour} />;
-        })}
-      </div>
+    
     </section>
   );
 };
